4b2b1dde42e55a158db736d93bc081629829bbae
dabutvin <butvinik@outlook.com>
2018-12-18 19:08:51 -0800

    use GITHUB_TOKEN to open a PR

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 const fs = require('fs')
 const path = require('path')
 const DocBuilder = require('tiny-attribution-generator/lib/docbuilder').default
-const TextRenderer = require('tiny-attribution-generator/lib/outputs/text').default;
-const ClearlyDefinedSource = require('tiny-attribution-generator/lib/inputs/clearlydefined').default;
-const JsonRenderer = require('tiny-attribution-generator/lib/outputs/json').default;
-const PackageLockSource = require('tiny-attribution-generator/lib/inputs/packagelock').default;
+const TextRenderer = require('tiny-attribution-generator/lib/outputs/text').default
+const ClearlyDefinedSource = require('tiny-attribution-generator/lib/inputs/clearlydefined').default
+const JsonRenderer = require('tiny-attribution-generator/lib/outputs/json').default
+const PackageLockSource = require('tiny-attribution-generator/lib/inputs/packagelock').default
+const request = require('superagent')
 
-const jsonRenderer = new JsonRenderer();
-const jsonBuilder = new DocBuilder(jsonRenderer);
-const clearlyDefinedBuilder = new DocBuilder(new TextRenderer());
+const jsonRenderer = new JsonRenderer()
+const jsonBuilder = new DocBuilder(jsonRenderer)
+const clearlyDefinedBuilder = new DocBuilder(new TextRenderer())
 
 const packageData = fs.readFileSync(
   path.join(__dirname, './package-lock.json')
-);
+)
 
-const packageLockSource = new PackageLockSource(packageData.toString());
-jsonBuilder
-  .read(packageLockSource)
-  .then(() => jsonBuilder.build())
-  .then(jsonOutput => {
-    const clearlydefinedInput = {
-      coordinates: jsonOutput.packages.map(
-        x =>
-          x.name.indexOf('/') > -1
-            ? `npm/npmjs/${x.name}/${x.version}`
-            : `npm/npmjs/-/${x.name}/${x.version}`
-      ),
-    };
-    return clearlyDefinedBuilder.read(
-      new ClearlyDefinedSource(JSON.stringify(clearlydefinedInput))
-    );
-  })
-  .then(() => {
-    const output = clearlyDefinedBuilder.build();
-    fs.writeFileSync('NOTICES', output)
-  });
+async function go() {
+
+  const packageLockSource = new PackageLockSource(packageData.toString())
+  await jsonBuilder.read(packageLockSource)
+  const jsonOutput = await jsonBuilder.build()
+  const clearlydefinedInput = {
+    coordinates: jsonOutput.packages.map(
+      x =>
+      x.name.indexOf('/') > -1 ?
+      `npm/npmjs/${x.name}/${x.version}` :
+      `npm/npmjs/-/${x.name}/${x.version}`
+    ),
+  }
+  await clearlyDefinedBuilder.read(new ClearlyDefinedSource(JSON.stringify(clearlydefinedInput)))
+  const output = clearlyDefinedBuilder.build()
+  console.log(output)
+
+  await request
+    .post(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/pulls`)
+    .auth(process.env.GITHUB_TOKEN, {
+      type: 'bearer'
+    })
+    .send({
+      "title": "Amazing new feature",
+      "body": "Please pull this in!",
+      "head": "notice",
+      "base": "master"
+    })
+}
+
+go()
