54f157648f1d1b2d08f293359d43074ffc391baf
dabutvin <butvinik@outlook.com>
2018-12-18 19:37:41 -0800

    update readme

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 Create a NOTICE attribution file based on your package-lock.json as a github action!
 
- - uses https://www.npmjs.com/package/tiny-attribution-generator and https://clearlydefined.io to generate
- - devDependencies filtered out (todo: add argument)
+- uses https://www.npmjs.com/package/tiny-attribution-generator and https://clearlydefined.io to generate
+- devDependencies filtered out (todo: add argument to bring them in)
+- optional argument for path to template to use for generation (todo)
+- optional argument for filename to use
 
 add ./github/main.workflow to your repo
 
@@ -15,5 +17,6 @@ workflow "NOTICE file generate" {
 
 action "Chive Action" {
   uses = "dabutvin/chive-action@master"
+  secrets = ["GITHUB_TOKEN"]
 }
 ```
--- index.js
+++ index.js
@@ -1,47 +1,57 @@
 const fs = require('fs')
 const path = require('path')
 const DocBuilder = require('tiny-attribution-generator/lib/docbuilder').default
-const TextRenderer = require('tiny-attribution-generator/lib/outputs/text').default
-const ClearlyDefinedSource = require('tiny-attribution-generator/lib/inputs/clearlydefined').default
-const JsonRenderer = require('tiny-attribution-generator/lib/outputs/json').default
-const PackageLockSource = require('tiny-attribution-generator/lib/inputs/packagelock').default
+const TextRenderer = require('tiny-attribution-generator/lib/outputs/text')
+  .default
+const ClearlyDefinedSource = require('tiny-attribution-generator/lib/inputs/clearlydefined')
+  .default
+const JsonRenderer = require('tiny-attribution-generator/lib/outputs/json')
+  .default
+const PackageLockSource = require('tiny-attribution-generator/lib/inputs/packagelock')
+  .default
 const request = require('superagent')
 
 const jsonRenderer = new JsonRenderer()
 const jsonBuilder = new DocBuilder(jsonRenderer)
 const clearlyDefinedBuilder = new DocBuilder(new TextRenderer())
 
-const packageData = fs.readFileSync(
-  path.join(__dirname, './package-lock.json')
-)
+const packageData = fs.readFileSync(path.join(__dirname, './package-lock.json'))
 
 async function go() {
-
   const packageLockSource = new PackageLockSource(packageData.toString())
   await jsonBuilder.read(packageLockSource)
   const jsonOutput = await jsonBuilder.build()
   const clearlydefinedInput = {
-    coordinates: jsonOutput.packages.map(
-      x =>
-      x.name.indexOf('/') > -1 ?
-      `npm/npmjs/${x.name}/${x.version}` :
-      `npm/npmjs/-/${x.name}/${x.version}`
-    ),
+    coordinates: jsonOutput.packages.map(x =>
+      x.name.indexOf('/') > -1
+        ? `npm/npmjs/${x.name}/${x.version}`
+        : `npm/npmjs/-/${x.name}/${x.version}`
+    )
   }
-  await clearlyDefinedBuilder.read(new ClearlyDefinedSource(JSON.stringify(clearlydefinedInput)))
+  await clearlyDefinedBuilder.read(
+    new ClearlyDefinedSource(JSON.stringify(clearlydefinedInput))
+  )
   const output = clearlyDefinedBuilder.build()
 
   // get a ref to the default branch
   const master = await request
-    .get(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/git/refs/heads/master`)
+    .get(
+      `https://api.github.com/repos/${
+        process.env.GITHUB_REPOSITORY
+      }/git/refs/heads/master`
+    )
     .auth(process.env.GITHUB_TOKEN, {
       type: 'bearer'
     })
     .send()
 
+  // todo: check if branch already exists and compare file output
+
   // create branch off master
   await request
-    .post(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/git/refs`)
+    .post(
+      `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/git/refs`
+    )
     .auth(process.env.GITHUB_TOKEN, {
       type: 'bearer'
     })
@@ -50,17 +60,22 @@ async function go() {
       sha: master.body.object.sha
     })
 
+  // todo: update vs create may be different endpoints
   // update notice file in the notices branch
   await request
-    .put(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/contents/NOTICES`)
+    .put(
+      `https://api.github.com/repos/${
+        process.env.GITHUB_REPOSITORY
+      }/contents/NOTICES`
+    )
     .auth(process.env.GITHUB_TOKEN, {
       type: 'bearer'
     })
     .send({
-      message: "update NOTICES",
+      message: 'update NOTICES',
       committer: {
-        name: "dabutvin",
-        email: "butvinik@outlook.com"
+        name: 'dabutvin',
+        email: 'butvinik@outlook.com'
       },
       content: Buffer.from(output).toString('base64'),
       branch: 'notices'
@@ -73,10 +88,10 @@ async function go() {
       type: 'bearer'
     })
     .send({
-      title: "NOTICE file updates",
-      body: "Please pull this in!",
-      head: "notices",
-      base: "master"
+      title: 'NOTICE file updates',
+      body: 'Please pull this in!',
+      head: 'notices',
+      base: 'master'
     })
 }
 
